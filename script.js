6ddef57b3eb5eac5d2d0384dcfa9424c3b738367
fjwrkn <113307371+fjwrkn@users.noreply.github.com>
2024-05-21 16:52:02 +0900

    Update script.js

--- script.js
+++ script.js
@@ -20,4 +20,14 @@ $('.workslist').on('click', function() {//タイトル要素をクリックし�
     $(this).addClass('close');//クラス名closeを付与
   }
 });
-  
+//アコーディオンをクリックした時の動作
+$('.read').on('click', function() {//タイトル要素をクリックしたら
+  var findElm = $(this).next(".readmore");//直後のアコーディオンを行うエリアを取得し
+  $(findElm).slideToggle();//アコーディオンの上下動作
+    
+  if($(this).hasClass('close')){//タイトル要素にクラス名closeがあれば
+    $(this).removeClass('close');//クラス名を除去し
+  }else{//それ以外は
+    $(this).addClass('close');//クラス名closeを付与
+  }
+});
